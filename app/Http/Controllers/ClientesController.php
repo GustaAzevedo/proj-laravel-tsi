7b2efef9d700e9185f9f396449e48406efc25a97
GustaAzevedo <69991055+GustaAzevedo@users.noreply.github.com>
2021-05-16 19:45:22 -0300

    clientes

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cliente;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasRoles;
 
 class ClientesController extends Controller
 {
@@ -16,10 +19,119 @@ class ClientesController extends Controller
 	}
 	*/
 
+    public function __construct()
+	{
+		$this->middleware('permission:cliente-list',['only' => ['index','show']]);
+		$this->middleware('permission:cliente-create',['only' => ['create','store']]);
+		$this->middleware('permission:cliente-edit',['only' => ['edit','update']]);
+		$this->middleware('permission:cliente-delete',['only' => ['destroy']]);
+	}
+
     public function listar()
     {
     	$clientes = Cliente::all();
 
     	return view('clientes.listar', ['clientes' => $clientes]);
+    }
+
+    public function index( Request $request)
+    {
+        $qtd_por_pagina = 5;
+
+        $data = Cliente::orderBy('id', 'DESC')->paginate($qtd_por_pagina);
+
+        return view('clientes.index',
+                compact('data'))->
+                    with('i', ($request->input('page', 1) - 1) * $qtd_por_pagina);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('clientes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,
+                        ['nome' => 'required',
+                        'email' => 'required|email|unique:users,email']);
+
+        $input = $request->all();
+
+        Cliente::create($input);
+
+        return redirect()->route('clientes.index')->with('success','Cliente criado com sucesso');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $cliente = Cliente::find($id);
+
+        return view('clientes.show', compact('cliente'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $cliente = Cliente::find($id);
+
+        return view('clientes.edit', compact('cliente'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,
+                        ['nome' => 'required',
+                        'email' => 'required|email|unique:users,email']);
+
+        $input = $request->all();
+
+        $cliente = Cliente::find($id);
+
+        $cliente->update($input);
+
+        return redirect()->route('clientes.index')->with('success', 'Cliente atualizado com sucesso');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Cliente::find($id)->delete();
+
+        return redirect()->route('clientes.index')->with('success','Cliente removido com sucesso');
     }
 }
---
+++ resources/views/avisos.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.externo')
+@section('title', 'Quadro de Avisos da Empresa')
+@section('sidebar')
+    @parent
+    <p>^ ^ Barra superior adicionada do layout pai/m&atilde;e ^ ^ </p>
+@endsection
+@section('content')
+    <p>Quadro de Avisos da Empresa</p>
+    <br><br>
+    <p>Olá, {{$nome}}! Veja abaixo aos avisos de hoje! </p>
+
+    @if($mostrar)
+
+    	@foreach($avisos as $aviso)
+    		<p>Aviso #{{$aviso['id']}}: {{$aviso['texto']}}</p>
+    	@endforeach
+
+    	<?php
+    	$var_do_arthur = 'Olá mundo';
+
+		foreach($avisos as $aviso){
+    		echo "<p>Aviso #{$aviso['id']}: {$aviso['texto']}</p>";
+		}
+    	?>
+
+    @else
+    	O aviso não será exibido!
+    @endif
+
+
+Var do Arthur: {{$var_do_arthur}}
+
+
+@endsection
